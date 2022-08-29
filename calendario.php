18d542e8318c08bca0cd66575b3a4b85501c3f23
aniscat <diana@gmail.com>
2022-08-28 21:54:48 -0500

    Uniendo las tablas categoria_evento e invitados a la tabla eventos:

--- calendario.php
+++ calendario.php
@@ -8,7 +8,15 @@
     try{
         // abrir la conexion
         require_once('includes/funciones/bd_conexion.php');
-        $sql = "SELECT * FROM eventos";
+        /*Para hacer join de tablas deben tener el mismo nombre de la tabla original y no de la que se incluye en la otra tabla */
+        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+        $sql .= " FROM eventos ";
+        $sql .= " INNER JOIN categoria_evento ";
+        /* Para saber que atributos deben ser iguales de dos tablas distintas*/
+        $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+        $sql .= "INNER JOIN invitados ";
+        $sql .= " ON eventos.id_inv = invitados.invitado_id ";
+        $sql .= " ORDER BY evento_id ";
         $resultado = $conn->query($sql);
 
     }catch(Exception $e){
